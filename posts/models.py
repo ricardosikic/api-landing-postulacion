a6480b99e1fe0efedaf492fca614cb4ac2384f02
ricardo <ricardo.sikic@gmail.com>
2019-09-23 19:06:31 -0300

    editado los campos de los modelos a blank=True asi no me da problemas con required fields en la peticion put desde el front

--- posts/models.py
+++ posts/models.py
@@ -3,22 +3,22 @@ from django.contrib.auth.models import User
 
 
 class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    content = models.TextField()
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
-    url = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='images/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=200, blank=True)
+    content = models.TextField(blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, default='', blank=True)
+    url = models.CharField(max_length=200, blank=True)
+    photo = models.ImageField(upload_to='images/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100)
+    category_name = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.category_name
